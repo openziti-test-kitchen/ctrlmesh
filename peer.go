76bbaf774bc27bb500557c7a0b11b9237006d868
Michael Quigley <michael@quigley.com>
2021-05-05 11:05:46 -0400

    Rough, minimal protobufs.

--- peer.go
+++ peer.go
@@ -2,14 +2,12 @@ package ctrlmesh
 
 import (
 	"github.com/openziti/foundation/channel2"
-	"github.com/openziti/foundation/identity/identity"
 	"github.com/openziti/foundation/transport"
-	"time"
 )
 
 type peer struct {
 	ctrl    channel2.Channel
-	id      *identity.TokenId
+	id      string
 	ads     []transport.Address
-	updated time.Time
+	updated uint32
 }
